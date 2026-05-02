4df0aa85c65cc0573c52ba006706cc3171da7e8b
shsohel <shsohel.tc@gmail.com>
2026-05-02 17:47:45 +0600

    branch route permission should be avaialbe

--- routes/branch.js
+++ routes/branch.js
@@ -21,7 +21,7 @@ router.use(express.json({ limit: "100kb" }));
 const { protect, authorize } = require("../middleware/auth");
 // protect all branch routes and allow only admin by default
 router.use(protect);
-router.use(authorize("admin branch"));
+router.use(authorize("admin", "branch"));
 
 // list (supports GET with query params and POST with body-filter via advancedResults)
 router
